81216a3626808a430485c70c1862d6d4e7e43a77
Wojciech Klause <wojtasxj@gmail.com>
2024-05-06 20:13:35 +0200

    search done

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,21 +4,27 @@ import CardForm from './../CardForm/CardForm';
 import { useSelector } from 'react-redux';
 
 const Column = props => {
-    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
     
-    const action = props.action;
-    
+    const cards = useSelector(state => {
+        if (state.searchString.trim() !== '') {
+            return state.cards.filter(card => card.columnId === props.id && card.title.toLowerCase().includes(state.searchString.toLowerCase()));
+        } else {
+            return state.cards.filter(card => card.columnId === props.id);
+        }
+    });
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}>
                 <span className={styles.icon + ' fa fa-' + props.icon} />
-                {props.title}</h2>
-                <ul className={styles.cards}>
-	                {cards.map(card => 
-                    <Card key={card.id} title={card.title} />)}
-                </ul>
-                <CardForm columnId={props.id} action={action} />
-                
+                {props.title}
+            </h2>
+            <ul className={styles.cards}>
+                {cards.map(card => 
+                    <Card key={card.id} title={card.title} />
+                )}
+            </ul>
+            <CardForm columnId={props.id} action={props.action} />
         </article>
     );
 };
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,35 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styles from './SearchForm.module.scss';
 import TextInput from './../TextInput/TextInput';
 import Button from './../Button/Buttonx';
 
+const updateSearchString = (searchString) => ({
+    type: 'UPDATE_SEARCHSTRING',
+    payload: searchString
+  });
+
 const SearchForm = () => {
+    const dispatch = useDispatch();
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        dispatch(updateSearchString(searchValue));
+    };
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
-            <Button>
+        <form className={styles.searchForm} onSubmit={handleFormSubmit}>
+            <TextInput
+                placeholder="Search..."
+                value={searchValue}
+                onChange={handleInputChange}
+            />
+            <Button type="submit">
                 <span className="fa fa-search" />
             </Button>
         </form>
